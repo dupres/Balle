0dd80ed4e31070fb73b88eb77b66e91659bc1e77
TisonAdrien <adrienbogalski@gmail.com>
2017-11-13 20:13:08 +0100

    Plus de réalisme
    On change la position à la fin de la fonction et on redéfinit les positions pour toucher les bords si la vitesse est trop grande.
    ( Pas de rebonds dans le vide ) Et de ce fait on améliore également le rendu lorsque la balle freine au sol

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // Initialisation des variables
 var balle = $("<div/>");
-var v_down = 10;
+var v_down = 1;
 var v_right = 10;
 var p_top = 10;
 var p_left = 10;
 var rebond_ratio = 2/3;
-var apesenteur = 1.5;
+var apesenteur = 7/4;
 var interval;
 var mouse_begin_x;
 var mouse_begin_y;
@@ -13,13 +13,16 @@ var mouse_end_x;
 var mouse_end_y;
 var change = false;
 
+
 var max_right = 500;
 var max_bot = 500;
+
 
 $(document).ready(function(){
 	//Redéfinition de la grandeur de la page
 	max_right = $(window).width();
 	max_bot = $(window).height();
+
 	//On définni le css de notre balle
 	balle.attr("id","balle")
 		.css({
@@ -61,54 +64,71 @@ $(document).ready(function(){
 		}
 	});
 
+
+
 	interval = setInterval(function(){
 		//On bouge la balle toutes les 30ms
 		move();
 	},30);
+
 });
+
+
 
 function move(){
 	//Si on ne change pas la vitesse de la balle
 	if(!change){
-		//On redéfinit la taille de la fenêtre 
+	    //On redéfinit la taille de la fenêtre 
 		max_right = $(window).width();
 		max_bot = $(window).height();
+
 		//La position de la balle est augmentée par celle de la vitesse
 		p_top = p_top + v_down;
 		p_left = p_left + v_right;
+
+		//On change la vitesse pour que la balle soit attiré vers le bas
+		// Si la balle est trop basse et que son rebond est faible, on stoppe les rebonds
+		if(v_down <= 1 && v_down >= -1 && p_top >= ($(window).height() - 52) ){
+			v_down = 0;
+			p_top = $(window).height() - 50;
+
+			// On fraine la balle quand elle est au sol
+			v_right = v_right * 42/43; // Parce que c'est joli
+		}else{
+			v_down = v_down + apesenteur;
+		}
+
+
+		// Ci-dessous les rebond_ratio servent à ralentir la balle au fur et à mesure de ses rebonds
+
+		//Si on est tout en bas on fait rebondir la balle
+		if(p_top + v_down + 50 > max_bot){
+			v_down = - v_down * rebond_ratio;
+			p_top = $(window).height() - 50;
+		}
+
+		//Si on est tout à droite on fait rebondir la balle
+		if(p_left + v_right + 50 > max_right){
+			v_right = - v_right * rebond_ratio;
+			p_left = $(window).width() - 50;
+		}
+
+		//Si on est tout en haut on fait rebondir encore
+		if(p_top + v_down < 0){
+			v_down = - v_down * rebond_ratio;
+			p_top = 0;
+		}
+
+		//Si on est tout à gauche on rebondit aussi
+		if(p_left + v_right < 0){
+			v_right = - v_right * rebond_ratio;
+			p_left = 0;
+		}
+
 		//On change la position au niveau du css
 		balle.css({
 			"top" : p_top+'px',
 			"left" : p_left+'px'
 		});
-
-		//On change la vitesse pour que la balle soit attiré vers le bas
-		// Si la balle est trop basse et que son rebond est faible, on stoppe les rebonds
-		if(v_down <= 1 && v_down >= -1 && p_top >= ($(window).height() - 59) ){
-			v_down = 0;
-			p_top = $(window).height() - 50;
-			// On fraine la balle quand elle est au sol
-			v_right = v_right * 42/44; // Parce que c'est joli
-		}else{
-			v_down = v_down + apesenteur;
-		}
-
-		// Ci-dessous les rebond_ratio servent à ralentir la balle au fur et à mesure de ses rebonds
-		//Si on est tout en bas on fait rebondir la balle
-		if(p_top + v_down + 50 >= max_bot){
-			v_down = - v_down * rebond_ratio;
-		}
-		//Si on est tout à droite on fait rebondir la balle
-		if(p_left + v_right + 50 >= max_right){
-			v_right = - v_right * rebond_ratio;
-		}
-		//Si on est tout en haut on fait rebondir encore
-		if(p_top + v_down <= 0){
-			v_down = - v_down * rebond_ratio;
-		}
-		//Si on est tout à gauche on rebondit aussi
-		if(p_left + v_right <= 0){
-			v_right = - v_right * rebond_ratio;
-		}
 	}
 }
